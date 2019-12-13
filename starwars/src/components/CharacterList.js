4374332670c5791fac71037b0f1479a4ebaf19d1
Handell Desulme <handelldesulme@Handells-MacBook-Pro.local>
2019-12-13 12:53:16 -0500

    adds next/prev page functionality with styled components

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,27 +1,46 @@
 import React, {useState, useEffect} from "react";
 import CharacterCard from "./CharacterCard";
-import {Container, Row} from 'reactstrap';
-
+import {Container, Row, Col} from 'reactstrap';
+import styled from 'styled-components'
 import axios from "axios"
 
+const Button = styled.button`
+    color: #fff;
+    text-transform: uppercase;
+    text-decoration: none;
+    background: #ed3330;
+    padding: 20px;
+    border-radius: 5px;
+    display: inline-block;
+    border: none;
+    transition: all 0.4s ease 0s;
+    &: hover {
+        color: pink;
+        border: 2px solid palevioletred;
+        background: #fff;
+    }
+`;
+
 function CharacterList() {
-    const starWarsEndpoint = ["https://swapi.co/api/people","https://lambda-swapi.herokuapp.com/api/people/"];
 
     //State
+    const [starWarsEndpoint, setStarWarsEndpoint] = useState("https://lambda-swapi.herokuapp.com/api/people/");
     const [characterData, setCharacterData] = useState([]);
+    const [pages, setPages] = useState([]); //pages = [previous, next]
 
 
     //Side Effects
     useEffect(() => {
-        axios.get(starWarsEndpoint[1])
+        axios.get(starWarsEndpoint)
             .then((response) => {
                 console.log(response);
                 setCharacterData(response.data.results);
+                setPages([response.data.previous,response.data.next]);
             })
             .catch((error) => {
                 console.log("Failed to retrieve data.")
             })
-    }, []);
+    }, [starWarsEndpoint]);
 
     //Return
     return (
@@ -30,6 +49,33 @@ function CharacterList() {
                 {characterData.map((charData, index) => (
                     <CharacterCard name={charData.name} birth_year={charData.birth_year} gender={charData.gender} height={charData.height} mass={charData.mass} hair_color={charData.hair_color} skin_color={charData.skin_color} eye_color={charData.eye_color}/>
                 ))}
+            </Row>
+            <br/>
+            <Row>
+                <Col>
+                    <Button onClick={() => {
+                        let prevPage = pages[0];
+                        if(prevPage) {
+                            console.log("Success! Prev: " + prevPage);
+                            setStarWarsEndpoint(prevPage);
+                        }
+                        else {
+                            console.log("Fail! Prev: " + prevPage);
+                        }
+                    }}>Previous Page</Button>
+                </Col>
+                <Col>
+                    <Button onClick={() => {
+                        let nextPage = pages[1];
+                        if(nextPage) {
+                            console.log("Success! Next: " + nextPage);
+                            setStarWarsEndpoint(nextPage);
+                        }
+                        else {
+                            console.log("Fail! Next: " + nextPage);
+                        }
+                    }}>Next Page</Button>
+                </Col>
             </Row>
         </Container>
     )
